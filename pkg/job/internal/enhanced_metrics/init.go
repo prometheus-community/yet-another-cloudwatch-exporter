0531dae362ac917eddd2bb3a68fb1b54262f4028
andriikushch <andrii.kushch@grafana.com>
2025-12-15 18:51:32 +0100

    Add elastiCache service
    Signed-off-by: andriikushch <andrii.kushch@grafana.com>

--- pkg/job/internal/enhanced_metrics/init.go
+++ pkg/job/internal/enhanced_metrics/init.go
@@ -1,6 +1,7 @@
 package enhanced_metrics
 
 import (
+	"github.com/prometheus-community/yet-another-cloudwatch-exporter/pkg/job/internal/enhanced_metrics/service/elasticache"
 	"github.com/prometheus-community/yet-another-cloudwatch-exporter/pkg/job/internal/enhanced_metrics/service/lambda"
 	"github.com/prometheus-community/yet-another-cloudwatch-exporter/pkg/job/internal/enhanced_metrics/service/rds"
 )
@@ -10,5 +11,8 @@ var DefaultRegistry = &Registry{}
 // init function to register enhanced metric services
 func init() {
 	// This is where you would register default enhanced metric services if any.
-	DefaultRegistry.Register(rds.NewRDSService(nil)).Register(lambda.NewLambdaService(nil))
+	DefaultRegistry.
+		Register(rds.NewRDSService(nil)).
+		Register(lambda.NewLambdaService(nil)).
+		Register(elasticache.NewElastiCacheService(nil))
 }
