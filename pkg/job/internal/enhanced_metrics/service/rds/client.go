bde6abb1b6cc2790af0ec2a708af001d9b3c3553
andriikushch <andrii.kushch@grafana.com>
2025-12-16 09:48:39 +0100

    Simplify client
    Signed-off-by: andriikushch <andrii.kushch@grafana.com>

--- pkg/job/internal/enhanced_metrics/service/rds/client.go
+++ pkg/job/internal/enhanced_metrics/service/rds/client.go
@@ -12,9 +12,13 @@ import (
 
 // todo: change logging to debug where appropriate
 
+type AwsClient interface {
+	DescribeDBInstances(ctx context.Context, params *rds.DescribeDBInstancesInput, optFns ...func(*rds.Options)) (*rds.DescribeDBInstancesOutput, error)
+}
+
 // AWSRDSClient wraps the AWS RDS client
 type AWSRDSClient struct {
-	client *rds.Client
+	client AwsClient
 }
 
 // NewRDSClientWithConfig creates a new RDS client with custom AWS configuration
@@ -24,40 +28,13 @@ func NewRDSClientWithConfig(cfg aws.Config) Client {
 	}
 }
 
-// DescribeDBInstancesInput contains parameters for describeDBInstances
-type DescribeDBInstancesInput struct {
-	DBInstanceIdentifier *string
-	Filters              []types.Filter
-	MaxRecords           *int32
-	Marker               *string
-}
-
-// DescribeDBInstancesOutput contains the response from describeDBInstances
-type DescribeDBInstancesOutput struct {
-	DBInstances []types.DBInstance
-	Marker      *string
-}
-
 // describeDBInstances retrieves information about provisioned RDS instances
-func (c *AWSRDSClient) describeDBInstances(ctx context.Context, input *DescribeDBInstancesInput) (*DescribeDBInstancesOutput, error) {
-	rdsInput := &rds.DescribeDBInstancesInput{}
-
-	if input != nil {
-		rdsInput.DBInstanceIdentifier = input.DBInstanceIdentifier
-		rdsInput.Filters = input.Filters
-		rdsInput.MaxRecords = input.MaxRecords
-		rdsInput.Marker = input.Marker
-	}
-
-	result, err := c.client.DescribeDBInstances(ctx, rdsInput)
+func (c *AWSRDSClient) describeDBInstances(ctx context.Context, input *rds.DescribeDBInstancesInput) (*rds.DescribeDBInstancesOutput, error) {
+	result, err := c.client.DescribeDBInstances(ctx, input)
 	if err != nil {
 		return nil, fmt.Errorf("failed to describe DB instances: %w", err)
 	}
-
-	return &DescribeDBInstancesOutput{
-		DBInstances: result.DBInstances,
-		Marker:      result.Marker,
-	}, nil
+	return result, nil
 }
 
 // DescribeAllDBInstances retrieves all DB instances by handling pagination
@@ -65,12 +42,12 @@ func (c *AWSRDSClient) DescribeAllDBInstances(ctx context.Context, logger *slog.
 	logger.Info("Looking for all DB instances")
 	var allInstances []types.DBInstance
 	var marker *string
-	var maxRecords int32 = 100
+	maxRecords := aws.Int32(100)
 
 	for {
-		output, err := c.describeDBInstances(ctx, &DescribeDBInstancesInput{
+		output, err := c.describeDBInstances(ctx, &rds.DescribeDBInstancesInput{
 			Marker:     marker,
-			MaxRecords: &maxRecords,
+			MaxRecords: maxRecords,
 		})
 		if err != nil {
 			return nil, err
@@ -85,20 +62,4 @@ func (c *AWSRDSClient) DescribeAllDBInstances(ctx context.Context, logger *slog.
 	}
 
 	return allInstances, nil
-}
-
-// describeDBInstance retrieves information about a specific DB instance
-func (c *AWSRDSClient) describeDBInstance(ctx context.Context, dbInstanceIdentifier string) (*types.DBInstance, error) {
-	output, err := c.describeDBInstances(ctx, &DescribeDBInstancesInput{
-		DBInstanceIdentifier: &dbInstanceIdentifier,
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	if len(output.DBInstances) == 0 {
-		return nil, fmt.Errorf("DB instance %s not found", dbInstanceIdentifier)
-	}
-
-	return &output.DBInstances[0], nil
 }
