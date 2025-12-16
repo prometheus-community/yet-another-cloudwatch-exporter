59a9a0221c9ad686352eb3cf87c01d819b0e2e63
andriikushch <andrii.kushch@grafana.com>
2025-12-16 10:09:02 +0100

    Simplify elasticache client
    Signed-off-by: andriikushch <andrii.kushch@grafana.com>

---
+++ pkg/job/internal/enhanced_metrics/service/elasticache/client_test.go
@@ -0,0 +1,105 @@
+package elasticache
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log/slog"
+	"reflect"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/elasticache"
+	"github.com/aws/aws-sdk-go-v2/service/elasticache/types"
+)
+
+func TestAWSElastiCacheClient_DescribeAllCacheClusters(t *testing.T) {
+	tests := []struct {
+		name    string
+		client  AwsClient
+		want    []types.CacheCluster
+		wantErr bool
+	}{
+		{
+			name: "success - single page",
+			client: &mockElastiCacheClient{
+				describeCacheClustersFunc: func(ctx context.Context, params *elasticache.DescribeCacheClustersInput, optFns ...func(*elasticache.Options)) (*elasticache.DescribeCacheClustersOutput, error) {
+					return &elasticache.DescribeCacheClustersOutput{
+						CacheClusters: []types.CacheCluster{
+							{CacheClusterId: aws.String("cluster-1")},
+						},
+						Marker: nil,
+					}, nil
+				},
+			},
+			want: []types.CacheCluster{
+				{CacheClusterId: aws.String("cluster-1")},
+			},
+			wantErr: false,
+		},
+		{
+			name: "success - multiple pages",
+			client: &mockElastiCacheClient{
+				describeCacheClustersFunc: func() func(ctx context.Context, params *elasticache.DescribeCacheClustersInput, optFns ...func(*elasticache.Options)) (*elasticache.DescribeCacheClustersOutput, error) {
+					callCount := 0
+					return func(ctx context.Context, params *elasticache.DescribeCacheClustersInput, optFns ...func(*elasticache.Options)) (*elasticache.DescribeCacheClustersOutput, error) {
+						callCount++
+						if callCount == 1 {
+							return &elasticache.DescribeCacheClustersOutput{
+								CacheClusters: []types.CacheCluster{
+									{CacheClusterId: aws.String("cluster-1")},
+								},
+								Marker: aws.String("marker1"),
+							}, nil
+						}
+						return &elasticache.DescribeCacheClustersOutput{
+							CacheClusters: []types.CacheCluster{
+								{CacheClusterId: aws.String("cluster-2")},
+							},
+							Marker: nil,
+						}, nil
+					}
+				}(),
+			},
+			want: []types.CacheCluster{
+				{CacheClusterId: aws.String("cluster-1")},
+				{CacheClusterId: aws.String("cluster-2")},
+			},
+			wantErr: false,
+		},
+		{
+			name: "error - API failure",
+			client: &mockElastiCacheClient{
+				describeCacheClustersFunc: func(ctx context.Context, params *elasticache.DescribeCacheClustersInput, optFns ...func(*elasticache.Options)) (*elasticache.DescribeCacheClustersOutput, error) {
+					return nil, fmt.Errorf("API error")
+				},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &AWSElastiCacheClient{
+				client: tt.client,
+			}
+			got, err := c.DescribeAllCacheClusters(context.Background(), slog.New(slog.NewTextHandler(io.Discard, nil)))
+			if (err != nil) != tt.wantErr {
+				t.Errorf("DescribeAllCacheClusters() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("DescribeAllCacheClusters() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+// mockElastiCacheClient is a mock implementation of AwsClient
+type mockElastiCacheClient struct {
+	describeCacheClustersFunc func(ctx context.Context, params *elasticache.DescribeCacheClustersInput, optFns ...func(*elasticache.Options)) (*elasticache.DescribeCacheClustersOutput, error)
+}
+
+func (m *mockElastiCacheClient) DescribeCacheClusters(ctx context.Context, params *elasticache.DescribeCacheClustersInput, optFns ...func(*elasticache.Options)) (*elasticache.DescribeCacheClustersOutput, error) {
+	return m.describeCacheClustersFunc(ctx, params, optFns...)
+}
