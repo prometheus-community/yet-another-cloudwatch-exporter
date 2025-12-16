0c7968064bf369f9d589a8467d7acdb433b384d8
andriikushch <andrii.kushch@grafana.com>
2025-12-16 11:55:30 +0100

    Refactor AWS service clients to use a generic Clients structure for improved code reuse and maintainability
    Signed-off-by: andriikushch <andrii.kushch@grafana.com>

---
+++ pkg/job/internal/enhanced_metrics/cache/clients.go
@@ -0,0 +1,68 @@
+package cache
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"sync"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/prometheus-community/yet-another-cloudwatch-exporter/pkg/job/internal/enhanced_metrics/config"
+	"github.com/prometheus-community/yet-another-cloudwatch-exporter/pkg/model"
+)
+
+// separator is NULL byte (0x00)
+const separator = "\x00"
+
+type Clients[T any] struct {
+	regionalClients map[string]T
+	clientsM        sync.RWMutex
+
+	buildClientFunc func(cfg aws.Config) T
+}
+
+func NewClients[T any](buildClientFunc func(cfg aws.Config) T) *Clients[T] {
+	return &Clients[T]{
+		buildClientFunc: buildClientFunc,
+		regionalClients: make(map[string]T),
+	}
+}
+
+func (c *Clients[T]) InitializeClient(_ context.Context, logger *slog.Logger, region string, role model.Role, configProvider config.RegionalConfigProvider) (T, error) {
+	var zero T
+	regionalConfig := configProvider.GetAWSRegionalConfig(region, role)
+	if regionalConfig == nil {
+		return zero, fmt.Errorf("could not get AWS config for region %s", region)
+	}
+
+	c.clientsM.Lock()
+	defer c.clientsM.Unlock()
+
+	key := c.getClientKey(region, role)
+	if c.regionalClients == nil {
+		c.regionalClients = make(map[string]T)
+	}
+	_, exists := c.regionalClients[key]
+	if !exists {
+		c.regionalClients[key] = c.buildClientFunc(*regionalConfig)
+	}
+
+	return c.regionalClients[key], nil
+}
+
+func (c *Clients[T]) GetClient(region string, role model.Role) T {
+	var zero T
+	c.clientsM.RLock()
+	defer c.clientsM.RUnlock()
+
+	key := c.getClientKey(region, role)
+	client, ok := c.regionalClients[key]
+	if !ok {
+		return zero
+	}
+	return client
+}
+
+func (c *Clients[T]) getClientKey(region string, role model.Role) string {
+	return region + separator + role.RoleArn + separator + role.ExternalID
+}
