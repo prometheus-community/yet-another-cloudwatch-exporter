0a1c7f1d68984926adaa14ca3b22c740cc1cc045
andriikushch <andrii.kushch@grafana.com>
2025-12-19 14:54:01 +0100

    Enhance documentation for MetricsService and Processor; clarify purpose and usage of methods
    Signed-off-by: andriikushch <andrii.kushch@grafana.com>

--- pkg/internal/enhancedmetrics/registry.go
+++ pkg/internal/enhancedmetrics/registry.go
@@ -11,22 +11,32 @@ import (
 	"github.com/prometheus-community/yet-another-cloudwatch-exporter/pkg/internal/enhancedmetrics/service/rds"
 )
 
+// DefaultRegistry is the default registry containing all built-in enhanced metrics services
+// It allows registering additional services if needed, or replacing existing ones, e.g. for testing purposes.
+//
+// Note:In the future, it can be removed in favor of being injected via dependency injection.
+// However, it will require changes in the YACE's API.
 var DefaultRegistry = (&Registry{}).
 	Register(rds.NewRDSService(nil)).
 	Register(lambda.NewLambdaService(nil)).
 	Register(dynamodb.NewDynamoDBService(nil)).
 	Register(elasticache.NewElastiCacheService(nil))
 
+// MetricsService represents an enhanced metrics service with methods to get its instance and namespace.
+// Services implementing this interface can be registered in the Registry.
 type MetricsService interface {
 	Instance() service.MetricsService
 	GetNamespace() string
 }
+
+// Registry maintains a mapping of enhanced metrics services by their namespaces.
 type Registry struct {
 	m sync.RWMutex
 
 	services map[string]func() service.MetricsService
 }
 
+// Register adds a new enhanced metrics service to the registry or replaces an existing one with the same namespace.
 func (receiver *Registry) Register(t MetricsService) *Registry {
 	receiver.m.Lock()
 	defer receiver.m.Unlock()
@@ -39,6 +49,7 @@ func (receiver *Registry) Register(t MetricsService) *Registry {
 	return receiver
 }
 
+// GetEnhancedMetricsService retrieves an enhanced metrics service by its namespace.
 func (receiver *Registry) GetEnhancedMetricsService(namespace string) (service.MetricsService, error) {
 	receiver.m.RLock()
 	defer receiver.m.RUnlock()
