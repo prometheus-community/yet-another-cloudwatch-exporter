63855cbae7bd36f60665d1393e40d088020b2d2f
andriikushch <andrii.kushch@grafana.com>
2025-12-19 17:20:45 +0100

    Refactor mock client methods to improve parameter clarity; remove unused parameters
    Signed-off-by: andriikushch <andrii.kushch@grafana.com>

--- pkg/internal/enhancedmetrics/config/provider.go
+++ pkg/internal/enhancedmetrics/config/provider.go
@@ -8,7 +8,6 @@ import (
 // RegionalConfigProvider is an interface for providing AWS regional configurations based on region and role.
 // Factory interface implementations should implement this interface in order to support enhanced metrics.
 type RegionalConfigProvider interface {
-
 	// GetAWSRegionalConfig returns the AWS configuration for a given region and role.
 	// It will be used to create AWS service clients for enhanced metrics processing.
 	GetAWSRegionalConfig(region string, role model.Role) *aws.Config
