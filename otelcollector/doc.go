19e541403347e957ae9defef74a72f52ab1a30a1
Arthur Silva Sens <arthursens2005@gmail.com>
2026-04-17 14:49:52 -0300

    Add package documentation
    Signed-off-by: Arthur Silva Sens <arthursens2005@gmail.com>

---
+++ otelcollector/doc.go
@@ -0,0 +1,18 @@
+// Copyright 2026 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package otelcollector exposes YACE as an OpenTelemetry Collector receiver.
+//
+// This package is experimental and its API and configuration may change as the
+// embedding support matures.
+package otelcollector
